7c5d12e2cfba6c13121e8ad4cc7d25588c7cbd97
Manas Gupta <manas.gupta_cs.aiml19@gla.ac.in>
2021-04-13 23:25:13 +0530

    create method to insert the data

--- src/implementation/MyBinarySearchTree.java
+++ src/implementation/MyBinarySearchTree.java
@@ -5,44 +5,44 @@ import myinterface.Node;
 
 public class MyBinarySearchTree<E extends Comparable<E>> implements BinarySearchTreeADT<E> {
     //complete this class
+    private Node root;
+    private int size;
 
+    public MyBinarySearchTree() {
+        root = null;
+        size = 0;
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
+    //
     @Override
     public void insert(E data) {
+        implementation.Node<E> node = new implementation.Node<>(data);
+        if (isEmpty()) {
+            root = node;
+        } else {
+            Node<E> temp = root;
+            Node<E> parent = null;
+            while (temp != null) {
+                parent = temp;
+                if (data.compareTo(temp.getData()) <= 0) {
+                    temp = temp.getLeft();
+                } else {
+                    temp = temp.getRight();
+                }
+            }
+            if (data.compareTo(parent.getData()) <= 0) {
+                parent.setLeft(node);
+            } else {
+                parent.setRight(node);
+            }
 
+        }
+        size++;
+        System.out.println("Node inserted");
     }
 
-    @Override
-    public boolean search(E searchElement) {
-        return false;
-    }
-
-    @Override
-    public void inOrder(Node<E> node) {
-
-    }
-
-    @Override
-    public void preOrder(Node<E> node) {
-
-    }
-
-    @Override
-    public void postOrder(Node<E> node) {
-
-    }
-
-    @Override
-    public void reverseInOrder(Node<E> node) {
-
-    }
-
-    @Override
-    public void delete(E data) {
-
-    }
-
-    @Override
-    public int height(Node<E> node) {
-        return 0;
-    }
 }
