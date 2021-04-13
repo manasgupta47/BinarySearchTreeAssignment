71f2f89bc951715128ad8ffa490fabe7904fc85d
Manas Gupta <manas.gupta_cs.aiml19@gla.ac.in>
2021-04-13 23:53:20 +0530

    Execution class for MyBinarySearchTree

--- src/main/MyMain.java
+++ src/main/MyMain.java
@@ -1,8 +1,22 @@
 package main;
 
+import implementation.MyBinarySearchTree;
+
 public class MyMain {
     public static void main(String[] args) {
         //create binary search tree
         // and perform insert, search, traversal, delete
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+        tree.insert(55);
+        tree.insert(12);
+        tree.insert(67);
+        tree.insert(25);
+        tree.insert(52);
+        tree.insert(43);
+        tree.inOrder(tree.getRoot());
+        tree.insert(34);
+        tree.inOrder(tree.getRoot());
+        System.out.println(tree.search(25));
+        System.out.println(tree.search(52));
     }
 }
